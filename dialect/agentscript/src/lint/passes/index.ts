898abfcb04317c99ea7035b7b4b3b4d01308d254
Setu Shah <setu.shah@salesforce.com>
2026-04-15 14:16:41 -0700

    🔥 fix: Remove `unused-variable` lint warnings (@W-22046277) (#287)
    GitOrigin-RevId: 81f356d936e94f6dfcfbbe3dbe1b298237df70d0

--- dialect/agentscript/src/lint/passes/index.ts
+++ dialect/agentscript/src/lint/passes/index.ts
@@ -16,7 +16,6 @@ import {
   positionIndexPass,
   unreachableCodePass,
   emptyBlockPass,
-  unusedVariablePass,
   expressionValidationPass,
   spreadContextPass,
 } from '@agentscript/language';
@@ -58,7 +57,6 @@ export function defaultRules(): LintPass[] {
     positionIndexPass(),
     unreachableCodePass(),
     emptyBlockPass(),
-    unusedVariablePass(),
     expressionValidationPass(),
     spreadContextPass(),
     // AgentScript analyzers
